bdd5cca83a65e634ebea632bc9692b357c1a5039
VariaQuality <dkswlsrud02@naver.com>
2024-11-05 16:48:27 +0900

    버튼 프로퍼티 미적용 수정

--- src/types/class-variance-authority.d.ts
+++ src/types/class-variance-authority.d.ts
@@ -1,4 +1,13 @@
 declare module 'class-variance-authority' {
-    export function cva(base: string, options?: any): any;
-    export type VariantProps<T> = any; 
-  }
+    export function cva(base: string, options?: {
+        variants?: Record<string, Record<string, string>>;
+        defaultVariants?: Record<string, string>;
+    }): {
+        (props?: Record<string, string>): string;
+        variants: Record<string, Record<string, string>>;
+        defaultVariants: Record<string, string>;
+    };
+    export type VariantProps<T> = T extends { variants: infer V }
+        ? { [K in keyof V]?: keyof V[K] }
+        : never;
+}
